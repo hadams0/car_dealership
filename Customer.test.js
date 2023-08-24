3a72bf3291f673f20f445280aadd3700ec37e662
hadams0 <adamshannah1@icloud.com>
2023-08-24 09:55:44 +0100

    made changes to dealership tests

---
+++ Customer.test.js
@@ -0,0 +1,3 @@
+const Car = require("./Car");
+const Dealership = require("./Dealership");
+const Customer = require("./Customer");
--- Dealership.test.js
+++ Dealership.test.js
@@ -5,52 +5,58 @@ beforeEach(() => {
   dealership = new Dealership("CarsBuy", 50);
 });
 
-test("can count all cars", () => {
-  const car1 = new Car("Ford", 100, "Manual");
-  const car2 = new Car("Volkswagon", 50, "Automatic");
+describe("Testing dealership methods", () => {
+  test("can count all cars", () => {
+    const car1 = new Car("Ford", 100, "Manual");
+    const car2 = new Car("Volkswagon", 50, "Automatic");
 
-  dealership.addCar(car1);
-  dealership.addCar(car2);
+    dealership.addCar(car1);
+    dealership.addCar(car2);
 
-  const expected = 2;
-  const actual = dealership.countAllCars();
+    const expected = 2;
+    const actual = dealership.countAllCars();
 
-  expect(actual).toBe(expected);
-});
+    expect(actual).toBe(expected);
+  });
 
-test("can add car to stock", () => {
-  const car = new Car("Ford", 100, "Manual");
+  test("can add car to stock", () => {
+    const car = new Car("Ford", 100, "Manual");
 
-  dealership.addCar(car);
+    dealership.addCar(car);
 
-  const expected = 1;
-  const actual = dealership.countAllCars();
+    const expected = 1;
+    const actual = dealership.countAllCars();
 
-  expect(actual).toBe(expected);
-});
+    expect(actual).toBe(expected);
+  });
 
-test("can return each car manufacturer", () => {
-  const car1 = new Car("Ford", 100, "Manual");
-  const car2 = new Car("Volkswagon", 50, "Automatic");
+  test("can return array of car manufacturers", () => {
+    const car1 = new Car("Ford", 100, "Manual");
+    const car2 = new Car("Volkswagon", 50, "Automatic");
 
-  dealership.addCar(car1);
-  dealership.addCar(car2);
+    dealership.addCar(car1);
+    dealership.addCar(car2);
 
-  expected = ["Ford", "Volkswagon"];
-  actual = dealership.getCarManufacturers();
+    expected = ["Ford", "Volkswagon"];
+    actual = dealership.getCarManufacturers();
 
-  expect(actual).toStrictEqual(expected);
-});
+    expect(actual).toStrictEqual(expected);
+  });
 
-test("can calculate toal value of all cars in stock", () => {
-  const car1 = new Car("Ford", 100, "Manual");
-  const car2 = new Car("Volkswagon", 50, "Automatic");
+  test("return nothing if manufacturer doesn't exist");
 
-  dealership.addCar(car1);
-  dealership.addCar(car2);
+  test("Return car from specific manufacturer");
 
-  expected = 150;
-  actual = dealership.totalValueOfAllCarsInStock();
+  test("can calculate total value of all cars in stock", () => {
+    const car1 = new Car("Ford", 100, "Manual");
+    const car2 = new Car("Volkswagon", 50, "Automatic");
 
-  expect(actual).toBe(expected);
+    dealership.addCar(car1);
+    dealership.addCar(car2);
+
+    expected = 150;
+    actual = dealership.totalValueOfAllCarsInStock();
+
+    expect(actual).toBe(expected);
+  });
 });
